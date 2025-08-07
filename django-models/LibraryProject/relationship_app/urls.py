659afb17129d4dff786e5c148ffe3f07142f7551
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-07 16:48:27 +0300

    Implement book management features: add, edit, and delete book views with permissions; create BookForm for validation and UI; update URLs and templates.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,6 +4,8 @@ from .views import CustomLoginView
 from .views import register
 from .views import logout_view
 from .views import admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book
+
 
 from django.urls import path
 
@@ -16,6 +18,9 @@ urlpatterns = [
     path('librarian/', librarian_view, name='librarian'),
     path('member/', member_view, name='member'),
     path('books/', list_books, name='list_books'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
     # urls needed for the checker
