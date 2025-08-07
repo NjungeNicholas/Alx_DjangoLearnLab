55ca9bc20f50d79a85bd69ab84224779646c5e04
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-07 14:51:45 +0300

    Refactor URL patterns to use direct imports from views in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
-from . import views
+from .views import list_books
+from .views import LibraryDetailView
 from django.urls import path
 
 # URL patterns for the relationship app
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
