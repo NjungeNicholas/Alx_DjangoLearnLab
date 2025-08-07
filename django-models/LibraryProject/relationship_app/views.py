da3941eb8781c6837d3093820332f7364391a91d
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-07 16:50:34 +0300

    Refactor import statements for clarity by separating user_passes_test and permission_required decorators

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .forms import BookForm 
 
 
