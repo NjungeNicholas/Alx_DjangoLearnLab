cd1b9bc8c9c1872f8251c6a604755b535ca98188
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-07 15:22:04 +0300

    Add user authentication views and templates for login, logout, and registration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,16 @@
 from .views import list_books
 from .views import LibraryDetailView
+from .views import CustomLoginView
+from .views import register
+from .views import logout_view
+
 from django.urls import path
 
 # URL patterns for the relationship app
 urlpatterns = [
+    path('login/', CustomLoginView, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout_view, name='logout'),
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+# function-based view to allow logging in users
+def CustomLoginView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+# Function to log out users
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+#function-based view to handle user registration
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
 
 # Function to list all books
 def list_books(request):
@@ -18,4 +51,5 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.object.books.all()
         return context
+
 
