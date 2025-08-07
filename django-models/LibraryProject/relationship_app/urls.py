bc70edf6c5560b560d710ac319eb8d3870a3449f
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-07 16:22:50 +0300

    Add user profile model and role-based views for admin, librarian, and member

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,7 @@ from .views import LibraryDetailView
 from .views import CustomLoginView
 from .views import register
 from .views import logout_view
+from .views import admin_view, librarian_view, member_view
 
 from django.urls import path
 
@@ -11,6 +12,9 @@ urlpatterns = [
     path('login/', CustomLoginView, name='login'),
     path('register/', register, name='register'),
     path('logout/', logout_view, name='logout'),
+    path('admin/', admin_view, name='admin'),
+    path('librarian/', librarian_view, name='librarian'),
+    path('member/', member_view, name='member'),
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import user_passes_test
 
 # function-based view to allow logging in users
 def CustomLoginView(request):
@@ -52,4 +53,30 @@ class LibraryDetailView(DetailView):
         context['books'] = self.object.books.all()
         return context
 
+# Role-based views
+# Helper functions to check roles
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
+
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
